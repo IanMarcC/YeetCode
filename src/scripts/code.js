1e168f81f39db8129e8c55ffae154274454ef05f
= <usrivastav10@gmail.com>
2019-02-17 02:47:50 -0800

    changed the challenge activities and complete page

--- src/scripts/code.js
+++ src/scripts/code.js
@@ -1,49 +1,86 @@
 
-$(document).ready();
-function submitter(language, message) {
-	//alert(language.value)
-	//alert(message.value)
-	alert("Thank you! Your code has been submitted for testing")
-	//testCases(language.value, message.value)
-	hello1('testing', message.value.split(""))
-}
+// function submitter(language, message) {
+// 	//alert(language.value)
+// 	//alert(message.value)
+// 	alert("Thank you! Your code has been submitted for testing")
+// 	//testCases(language.value, message.value)
+// 	hello1('testCases', message.value.split(""))
+// }
 
 
 
 //		$("#testing").innerHTML += value
 
+let testCaseNumber = 0;
+const numParams = 2;
+function parser() {
+	var arr = window.location.href.split('?');
+	obj = arr.pop()
+	fields = obj.split("+")
+	fields = fields.map(field =>{
+		 return (field.split("%20")).join(" ");
+	})
+	//fields[1] = (fields[1].split("%20")).join(" ")
+	console.log(fields)
+	console.log(document.getElementById("challenge"))
+	document.getElementById("challenge").getElementsByTagName("H1")[0].innerHTML = fields[0];
+	document.getElementById("challenge").getElementsByTagName("H2")[0].innerHTML = fields[1];
+	params()
+}
+
+
+
+//creates an unordered list for each test case
+function params() {
+	f = document.getElementById("cases")
+	let Li = document.createElement("Li");
+	Li.setAttribute("class", "enclosingCase");
+testCaseNumber+=1
+	Li.innerHTML+=testCaseNumber;
+	//Li.setAttribute("class", "box")
+	for (var i = 0; i<numParams; i++) {
+		let li = document.createElement("li")
+		li.setAttribute("class", "box")
+		inp = document.createElement("input");
+		inp.setAttribute('type', "text")
+		li.append(inp)
+		//li.setAttribute('class', "testCase")
+		Li.append(li)
+	}
+	//Li.append(document.getElementsByClassName("buffered")[0])
+	Li.innerHTML+="<br>"
+	f.append(Li)
+
+
+}
+
+
+function submitter(ol) {
+	console.log(ol)
+	let cases = ol.getElementsByClassName("box")
+	let case = []
+	let evals = []
+	for (var i = 0; i<cases.length; i+=numParams) {
+		console.log()
+	}
+
+}
+
 function hello1(id, data) {
+	document.getElementById(id).innerHTML = "";
 	f = document.getElementById(id)
 	// data.forEach(value => {
 	// 	$(f).append('<li class="testCase">' + value + "</li>")
 	// 	console.log()
 	// });
 	for (var i = 1; i<=data.length; i++) {
-		$(f).append('<li class="testCase">Test Case ' + i + ": " + data[i-1] + "</li>")
+		var item = document.createElement("li");
+		item.setAttribute('class', 'testCase')
+		item.textContent = "Test Case " + i + ": " + data[i-1]
+		$(f).append(item)
+		//$(f).append('<li class="testCase">Test Case ' + i + ": " + data[i-1] + "</li>")
 	}
+	document.getElementById("list").style.height = "100%;"
 }
 
-
-function testCases(language, message) {
-	//api call, returns some sort of json
-	var request = new XMLHttpRequest();
-
-	request.open('GET', URL, true);
-	request.onload = function () {
-
-	  // Begin accessing JSON data here
-	  var data = JSON.parse(this.response);
-
-	  if (request.status >= 200 && request.status < 400) {
-	    data.forEach(movie => {
-	      console.log(movie.title);
-	    });
-	  } else {
-	    console.log('error');
-	  }
-	}
-
-	request.send();
-
-
-}
+$(document).ready(parser);
--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,8 +1,11 @@
-function expandList(){
+function expandList(problem){
     var dl = document.getElementById("problem-list");
-    var problem = document.getElementById("problem");
-    var description = document.getElementById("problem");
+    console.log(problem)
+
+
     var dt = document.createElement("dt");
+    dt.setAttribute("id", problem.name);
+    dt.setAttribute("class", "problemListing")
     var a = document.createElement("a");
     var buffer = document.createElement("div");
     buffer.setAttribute('class','buffer');
@@ -10,14 +13,73 @@ function expandList(){
     Needs to get object containing the description term AND the description itself, appending the description after the description terms
     */
     var dd = document.createElement("dd");
-    dd.setAttribute('id',problem.value);
-    a.textContent = problem.value;
-    a.setAttribute('href',"code.html");
+    dd.setAttribute('id',problem.description);
+    a.textContent = problem.name;
+    a.setAttribute('href',"code.html?" + problem.name + "+" + problem.description + "+" + problem.function_body);
     dt.appendChild(a);
+    var button = document.createElement("button");
+    button.setAttribute("value", "UpVote")
+    button.setAttribute("class", "button")
+    button.setAttribute("onclick", 'toggleUpVote(document.getElementById().parentElement);')
     /*
     dt.appendChild(document.createTextNode(problem.value));
     */
+    dt.append(button)
     dl.appendChild(dt);
     dl.appendChild(dd);
+
     dl.appendChild(buffer);
 }
+
+
+function toggleUpVote(element) {
+  console.log(element)
+
+}
+
+
+
+function getChallenges() {
+
+    let URL = "/challenges"
+    console.log(URL)
+    var request = new XMLHttpRequest();
+    console.log(request)
+    request.open('GET', URL, true);
+    request.onload = function () {
+
+      // Begin accessing JSON data here
+      var data = JSON.parse(this.response);
+      console.log(data)
+
+
+      if (request.status >= 200 && request.status < 400) {
+        data = data['problems'].filter(problem => {
+          //console.log(problem.Body.language);
+          //console.log(document.getElementById("language").value);
+          return problem.Body.language === document.getElementById("language").value;
+        })
+        console.log(data)
+        data.sort(function(a,b) {
+          return a.votes-b.votes;
+        })
+        data.forEach(problem => {
+          //console.log(problem.Body);
+          expandList(problem.Body);
+        });
+      } else {
+        console.log('error');
+      }
+   }
+
+
+    request.send();
+}
+
+function newChallenges() {
+  document.getElementById("problem-list").innerHTML = "";
+  getChallenges()
+}
+
+
+$(document).ready(getChallenges());
